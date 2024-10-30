d90aa96039127f5dd7d0e586654e1226ff147ed8
Thabang H Maphangisane <102468265+Maphangisane@users.noreply.github.com>
2024-10-30 17:19:50 +0200

    feat: server and app setup to seperate concerns and for testing

---
+++ server.ts
@@ -0,0 +1,10 @@
+import app from './app.js';
+import chalk from 'chalk';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+	console.log(chalk.magenta.bold(`Server is running at http://localhost:${PORT}`));
+});
